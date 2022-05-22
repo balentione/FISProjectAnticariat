5f4905121ebbf84ce7aab0d7891a713e60b9cb5b
ClaudiuPrivantu <101561385+ClaudiuPrivantu@users.noreply.github.com>
2022-05-22 17:19:43 +0300

    PFA-41
    Am creat JAR File-ul.

---
+++ target/run.sh
@@ -0,0 +1,1 @@
+java -jar --module-path "/Library/Java/JavaVirtualMachines/javafx-sdk-18/lib" --add-modules javafx.controls,javafx.fxml simple-registration-example-0.1.0-SNAPSHOT-jar-with-dependencies.jar
